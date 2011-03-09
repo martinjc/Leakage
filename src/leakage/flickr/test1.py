609ace02c277aec19e540ad82a19de7877744325
Ian Cooper <i.m.cooper@cs.cf.ac.uk>
2011-03-09 22:52:14 +0000

    initial trial of scraper

--- src/leakage/flickr/test1.py
+++ src/leakage/flickr/test1.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
-"""Usage: python tags2set.py [OPTIONS] TAGS
-TAGS is a space delimited list of tags
 
-OPTIONS:
-  -e address or --email address
-  -p password or --password password
-  -t title or --title title
-  -d description or --description description  [optional]
-  Enclose title and description in 'quotes' for multiple words
-"""
-
-__author__ = "James Clarke <james@jamesclarke.info>"
-__version__ = "$Rev$"
+__author__ = "Ian Cooper"
+__version__ = "0.0.1"
 __date__ = "$Date$"
-__copyright__ = "Copyright 2004 James Clarke"
+__copyright__ = "Copyright 2011"
 
 import sys
 import flickr
 
+def contacts_getPublicList(user_id):
+    """Gets the contacts (Users) for the user_id"""
+    method = 'flickr.contacts.getPublicList'
+    data = flickr._doget(method, auth=False, user_id=user_id)
+
+    try:
+      if isinstance(data.rsp.contacts.contact, list):
+          return [User(user.nsid, username=user.username) \
+                  for user in data.rsp.contacts.contact]
+
+    except AttributeError:
+      return "No users in the list"
+    except:
+      return "Unknown error"
+
 def get_user_contacts(userID):
     """           """
-    contacts = flickr.contacts_getPublicList(user_id=userID)
+    contacts = flickr.contacts_getPublicList(user_id=userID) 
+#    contacts = contacts_getPublicList(user_id=userID) 
+ 
     return contacts
 
 def main(*argv):
@@ -29,6 +36,8 @@ def main(*argv):
 
     contacts = get_user_contacts(userID)
     print "user %s has %s contacts" % (userID, len(contacts)) 
+    for user in contacts:
+		print "user %s name %s" % (user.id, user.username)
     
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
